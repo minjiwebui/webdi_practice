8287b13c60ae392a042833fe7d16003218de8ea3
minjiwebui <minamall376@gmail.com>
2023-06-12 22:49:49 +0900

    z

--- script/script_A-1.js
+++ script/script_A-1.js
@@ -17,33 +17,6 @@ $('header nav > ul').mouseenter(function(){
       num = 1;
     }
   }, 3000);
-  // 슬라이드 가로형
-  var num = 1;
-  
-  setInterval(function () {
-    if (num < 3) {
-      $(".slide").animate({ left: "-=" + 1200 }, "slow");
-      num++;
-    } else {
-      $(".slide").animate({ left: 0 }, "fast");
-      num = 1;
-    }
-  }, 3000);
-  // 슬라이드 fadein, fadeout
-  var num = 0;
-  
-  $(".slide").fadeout(0);
-  $(".slide").eq(num).fadein(0);
-  
-  setInterval(function () {
-    if (num < 2) {
-      num++;
-    } else {
-      num = 0;
-    }
-    $(".slide").fadeout("slow");
-    $(".slide").eq(num).fadein("slow");
-  }, 3000);
   // 탭메뉴 가로형-인덱스번호
   $(".tab ul li").click(function(){
     var tabindex = $(this).index();
@@ -57,12 +30,3 @@ $('header nav > ul').mouseenter(function(){
   $(".popup ").click(function(){
     $(".popup").removeClass("on");
   });
-  // 패밀리 사이트(선택 버튼)
-  <div class="f_fam">
-       <select class="f_family">
-            <option id="fam1">패밀리사이트</option>
-            <option id="fam2">패밀리사이트1</option>
-            <option id="fam3">패밀리사이트2</option>
-            <option id="fam4">패밀리사이트3</option>
-       </select>
-  </div>
